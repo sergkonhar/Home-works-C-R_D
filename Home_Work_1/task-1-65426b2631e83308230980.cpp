161482a95cff66bcd505fe14c1d2c59e723c7769
Sergey <sergkonhar@gmail.com>
2023-11-16 18:22:19 +0200

    add home work 1

---
+++ Home_Work_1/task-1-65426b2631e83308230980.cpp
@@ -0,0 +1,13 @@
+// Task#1.cpp : Home work phrase + triangle
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "I Love C++\n\n\n";
+    std::cout << "*\n**\n* *\n*  *\n*   *\n*    *\n*     *\n* * * **";
+
+}
+
+
